61906d5db2834516a5e2bb8fd9321f0f694c3f60
Raja-25 <your-email@example.com>
2026-01-29 07:48:41 +0530

    added skills rating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ScreeningComponent } from './features/screening/screening';
 import { DropoutComponent } from './features/dropout/dropout';
 import { DashboardComponent } from './features/dashboard/dashboard';
+import { SkillRatingsComponent } from './features/skill-ratings/skill-ratings';
 
 export const routes: Routes = [
   { path: '', component: ScreeningComponent },
   { path: 'screening', component: ScreeningComponent },
   { path: 'dropout', component: DropoutComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'skillratings', component: SkillRatingsComponent },
   { path: '**', redirectTo: '' }
 ];
